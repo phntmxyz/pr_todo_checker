3cbb046974ea6d584b05da0fdfe05ca22bd1c9e6
Maikel Rehl <maikel@phntm.xyz>
2024-03-28 16:22:07 +0100

    Initial commit

---
+++ __test__/main.test.ts
@@ -0,0 +1,30 @@
+import { findTodos } from '../src/main' // replace with the actual path to the function
+
+const __diff = `
++ any text before   //       TODO - upper case with much space
++ // todo - lower case with space
++ //todo - lower case no space
++
++ /*
++  * todo - In comment block
++ #  todo - comment with hashtag
+- #  todo - removed comment with hashtag
+- removed non todo line
++ added non todo line
+`
+
+describe('extractTodos', () => {
+  it('should correctly extract todos', () => {
+    const { newTodos, removedTodos } = findTodos(__diff)
+
+    expect(newTodos).toEqual([
+      'TODO - upper case with much space',
+      'todo - lower case with space',
+      'todo - lower case no space',
+      'todo - In comment block',
+      'todo - comment with hashtag'
+    ])
+
+    expect(removedTodos).toEqual(['todo - removed comment with hashtag'])
+  })
+})
---
+++ src/main.ts
@@ -0,0 +1,138 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+export async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+
+    const base = core.getInput('base')
+    const head = core.getInput('head')
+
+    const baseRef = base || github.context.payload.pull_request?.base.ref
+    const headRef = head || github.context.payload.pull_request?.head.ref
+
+    if (!baseRef || !headRef) {
+      throw new Error('Base or head ref not found')
+    }
+
+    const prDiff = await getPrDiff(octokit, baseRef, headRef)
+    const { newTodos, removedTodos } = findTodos(prDiff)
+
+    const comment = generateComment(newTodos, removedTodos)
+
+    await commentPr(octokit, comment)
+    core.setOutput('comment', comment)
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      console.log(error)
+      core.setFailed('An unexpected error occurred')
+    }
+  }
+}
+
+async function getPrDiff(
+  octokit: ReturnType<typeof github.getOctokit>,
+  base: string,
+  head: string
+): Promise<string> {
+  const { owner, repo } = github.context.repo
+
+  const response = await octokit.rest.repos.compareCommitsWithBasehead({
+    owner,
+    repo,
+    basehead: `${base}...${head}`
+  })
+
+  return response?.data?.files?.map(file => file.patch).join('\n') ?? ''
+}
+
+export function findTodos(prDiff: string): {
+  newTodos: string[]
+  removedTodos: string[]
+} {
+  const newTodos: string[] = []
+  const removedTodos: string[] = []
+
+  const lines = prDiff.split('\n')
+  for (const line of lines) {
+    if (line.startsWith('+')) {
+      const todo = getTodoIfFound(line)
+      if (todo.length > 0) newTodos.push(todo.trim())
+    } else if (line.startsWith('-')) {
+      const todo = getTodoIfFound(line)
+      if (todo.length > 0) removedTodos.push(todo.trim())
+    }
+  }
+
+  console.log('New TODOs found in this PR:', newTodos)
+  console.log('Solved TODOs found in this PR:', removedTodos)
+
+  return { newTodos, removedTodos }
+}
+
+function getTodoIfFound(line: string): string {
+  const regex = /[/*#]+.*(TODO.*)/gi
+  const matches = [...line.matchAll(regex)]
+  if (matches === undefined || matches.length === 0) return ''
+  return matches[0][1]
+}
+
+function generateComment(newTodos: string[], removedTodos: string[]): string {
+  let comment = 'New TODOs found in this PR:\n'
+  for (const todo of newTodos) {
+    comment += `- [ ] ${todo}\n`
+  }
+  comment += '\nSolved TODOs found in this PR:\n'
+  for (const todo of removedTodos) {
+    comment += `- [x] ${todo}\n`
+  }
+
+  console.log('Comment:', comment)
+  return comment
+}
+
+async function commentPr(
+  octokit: ReturnType<typeof github.getOctokit>,
+  comment: string
+): Promise<void> {
+  const { owner, repo } = github.context.repo
+  const issueNumber = github.context.payload.pull_request?.number
+
+  if (!issueNumber) throw new Error('Issue number not found')
+
+  // Get all comments on the pull request
+  const { data: comments } = await octokit.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: issueNumber
+  })
+
+  // Find the comment created by this action
+  const existingComment = comments.find(
+    entry =>
+      entry.user?.login === 'github-actions[bot]' &&
+      entry.body?.startsWith('New TODOs found in this PR:')
+  )
+
+  // If the comment exists, update it; otherwise, create a new comment
+  if (existingComment) {
+    console.log(`Update existing comment #${existingComment.id}`)
+    await octokit.rest.issues.updateComment({
+      owner,
+      repo,
+      comment_id: existingComment.id,
+      body: comment
+    })
+  } else {
+    console.log(`Create new comment`)
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: comment
+    })
+  }
+}
